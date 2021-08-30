4de826bbce48c4056f83ac044386220c303a5a44
Sai Teja Malladi <saiteja.malladi@gmail.com>
2021-08-30 21:57:34 +0800

    API base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
   validateStatus: (status) => status < 500,
 });
